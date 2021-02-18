917801c54e9121ca3d2282c91c46f92df63184b7
fell8000 <63782897+fell8000@users.noreply.github.com>
2021-02-18 18:36:26 +0900

    Add files via upload

--- Max_num.py
+++ Max_num.py
@@ -3,6 +3,9 @@ n,m,k=map(int,input().split())
 data=list(map(int,input().split()))
 
 data.sort()
-ans=data[-1]*k*(m//k)+data[-2]*(m%k)
+#제일 큰 수 
+count=(k*(m//(k+1))+m%(k+1))
+ans=data[-1]*count
+ans+=data[-2]*(m-count)
 
 print(ans)
